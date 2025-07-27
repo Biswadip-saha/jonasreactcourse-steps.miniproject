9b7d235fc744ee16ce2aa7d3ec113e2ab0b608e8
Biswadip-saha <biswadip.learn@gmail.com>
2025-07-28 00:48:48 +0530

    add: useState()

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ const messages = ["Learn React ⚛️", "Apply for jobs 💼", "Invest your new 
 
 export default function App() {
 	const [step, setStep] = useState(1);
+	const [isOpen, setIsOpen] = useState(true);
 
 	function handlePrevious() {
 		if (step > 1) {
@@ -18,23 +19,28 @@ export default function App() {
 	}
 
 	return (
-		<div className="steps">
-			<div className="numbers">
-				<div className={step >= 1 ? "active" : ""}>1</div>
-				<div className={step >= 2 ? "active" : ""}>2</div>
-				<div className={step >= 3 ? "active" : ""}>3</div>
-			</div>
-			<p className="message">
-				Step {step} : {messages[step - 1]}
-			</p>
-			<div className="buttons">
-				<button style={{ backgroundColor: "#7950f2", color: "#ffffff" }} onClick={handlePrevious}>
-					Previous
-				</button>
-				<button style={{ backgroundColor: "#7950f2", color: "#ffffff" }} onClick={handleNext}>
-					Next
-				</button>
-			</div>
-		</div>
+		<>
+			<button className="close" onClick={()=>setIsOpen(!isOpen)}>&times;</button>
+			{isOpen && (
+				<div className="steps">
+					<div className="numbers">
+						<div className={step >= 1 ? "active" : ""}>1</div>
+						<div className={step >= 2 ? "active" : ""}>2</div>
+						<div className={step >= 3 ? "active" : ""}>3</div>
+					</div>
+					<p className="message">
+						Step {step} : {messages[step - 1]}
+					</p>
+					<div className="buttons">
+						<button style={{ backgroundColor: "#7950f2", color: "#ffffff" }} onClick={handlePrevious}>
+							Previous
+						</button>
+						<button style={{ backgroundColor: "#7950f2", color: "#ffffff" }} onClick={handleNext}>
+							Next
+						</button>
+					</div>
+				</div>
+			)}
+		</>
 	);
 }
